ef49ab73d64e03d41c3888f15c3905dd344026e1
johanR94 <radegondejohan@outlook.fr>
2025-05-26 14:37:25 +0200

    Correction deploy

--- src/Api/apiLogements.js
+++ src/Api/apiLogements.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-useless-catch */
 //! Récuperation des données de logement depuis le fichier JSON
 export async function fetchLogementData() {
-    try {
-        // Peremet de différencier la requête selon si l'url dispose d'une ID
-        const url =`/src/Data/Accomodation.json`;
-        const response = await fetch(url);
+  try {
+    // Peremet de différencier la requête selon si l'url dispose d'une ID
+    const url = `/src/Data/Accomodation.json`;
+    const response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch logement data');
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        throw error;
+    if (!response.ok) {
+      throw new Error("Failed to fetch logement data");
     }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
-
-
